b9a3c0ad36644db0ea2ec5cc3b66b25be98b4217
Smail LOUNES <slounes@wissal-sys.com>
2020-03-18 00:28:40 +0100

    Adding script to inject secrets and kubeconfig file

---
+++ build-scripts/inject-secrets.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+mkdir ~/.kube
+mv ./build-scripts/kubeconfig ~/.kube/config
+ 
+#decrypt the large secrets
+openssl aes-256-cbc -K $encrypted_8ff0da25ff85_key -iv $encrypted_8ff0da25ff85_iv -in large-secrets.txt.enc -out build-scripts/large-secrets.txt -d
+ 
+# run the script to get the secrets as environment variables
+source ./build-scripts/large-secrets.txt
+export $(cut -d= -f1 ./build-scripts/large-secrets.txt)
+ 
+ 
+# Set kubernetes secrets
+./kubectl config set clusters.udagramk8s.kops.ucci.uk.certificate-authority-data $CERTIFICATE_AUTHORITY_DATA
+./kubectl config set users.udagramk8s.kops.ucci.uk.client-certificate-data "$CLIENT_CERTIFICATE_DATA"
+./kubectl config set users.udagramk8s.kops.ucci.uk.client-key-data "$CLIENT_KEY_DATA"
+./kubectl config set users.udagramk8s.kops.ucci.uk.password "$KUBE_PASSWORD"
+./kubectl config set users.udagramk8s.kops.ucci.uk.net-basic-auth.password "$KUBE_PASSWORD"
+ 
+# set AWS secrets
+mkdir ~/.aws
+touch ~/.aws/credentials
+echo '[default]' >> ~/.aws/credentials
+echo "aws_access_key_id = $AWS_KEY">> ~/.aws/credentials
+echo "aws_secret_access_key = $AWS_SECRET_KEY" >> ~/.aws/credentials
+ 
+# set AWS region
+touch ~/.aws/config
+echo '[default]' >> ~/.aws/config
+echo "output = json">> ~/.aws/config
+echo "region = {your-aws-region}" >> ~/.aws/config
