f8e09fc4b50c283ceb929a082ce6317033405106
Smail LOUNES <slounes@wissal-sys.com>
2020-03-20 12:29:40 +0100

    test

--- build-scripts/deploy-k8s.sh
+++ build-scripts/deploy-k8s.sh
@@ -3,22 +3,20 @@ GREEN='\033[0;32m'
 NC='\033[0;0m'
 export PATH=$PATH:$(pwd)
 
-echo -e "Deploying a new Deployment object"
-kubectl apply -f ./backend-feed-deployment.yaml
 
-# echo -e "${GREEN}==== Deploying RBAC role ====${NC}"
-# cd ./rbac/
-# for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
-# echo -e "${GREEN}==== Done deploying RBAC role ====${NC}"
-# echo ''
+echo -e "${GREEN}==== Deploying RBAC role ====${NC}"
+cd ./rbac/
+for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
+echo -e "${GREEN}==== Done deploying RBAC role ====${NC}"
+echo ''
 
-# echo -e "${GREEN}==== Deploying iam role ====${NC}"
-# cd ../kube2iam/
-# for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
-# echo -e "${GREEN}==== Done deploying iam role ====${NC}"
-# echo ''
-# echo -e "${GREEN}==== Deploying external dns ====${NC}"
-# cd ../external_dns/
-# for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
-# echo -e "${GREEN}==== Done deploying external dns ====${NC}"
-# echo ''
+echo -e "${GREEN}==== Deploying iam role ====${NC}"
+cd ../kube2iam/
+for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
+echo -e "${GREEN}==== Done deploying iam role ====${NC}"
+echo ''
+echo -e "${GREEN}==== Deploying external dns ====${NC}"
+cd ../external_dns/
+for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
+echo -e "${GREEN}==== Done deploying external dns ====${NC}"
+echo ''
