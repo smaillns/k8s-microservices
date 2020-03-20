35526d9868f6cb1930718dd24784a831745dc8fa
Smail LOUNES <slounes@wissal-sys.com>
2020-03-20 11:01:34 +0100

    test

--- build-scripts/deploy-k8s.sh
+++ build-scripts/deploy-k8s.sh
@@ -3,6 +3,9 @@ GREEN='\033[0;32m'
 NC='\033[0;0m'
 export PATH=$PATH:$(pwd)
 
+echo -e "Deploying a new Deployment object"
+kubectl apply -f backend-feed-deployment.yaml --validate=false
+
 echo -e "${GREEN}==== Deploying RBAC role ====${NC}"
 cd ./rbac/
 for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
